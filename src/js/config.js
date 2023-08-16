b944bd72de85f176b2f716f658e82e4878cf59cf
ritish65 <ritishkhandelwal89@gmail.com>
2023-08-16 07:57:56 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'f54719e1-2ea5-4a7b-8071-5d42d406b425';
+export const MODAL_CLOSE_SEC = 2.5;
